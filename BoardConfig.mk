09edc1f30b8cefc458722109edb6042e824bbd30
Alberto97 <albertop2197@gmail.com>
2015-08-25 20:45:21 +0200

    titan: Move props to the common device tree
     * Rename LOCAL_PATH to DEVICE_PATH
     * Use $(DEVICE_PATH) instead of hardcoded path
    
    Change-Id: I2f584d57a51e0bc5cb563cb2e692e6b95e8cc6f4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,21 +16,21 @@
 # inherit from common msm8226-common
 -include device/motorola/msm8226-common/BoardConfigCommon.mk
 
-LOCAL_PATH := device/motorola/titan
+DEVICE_PATH := device/motorola/titan
 
 # Vendor Unification Init
 TARGET_UNIFIED_DEVICE := true
 TARGET_INIT_VENDOR_LIB := libinit_msm
-TARGET_LIBINIT_DEFINES_FILE := device/motorola/titan/init/init_titan.c
+TARGET_LIBINIT_DEFINES_FILE := $(DEVICE_PATH)/init/init_titan.c
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := titan,titan_umts,titan_udstv,titan_umtsds,titan_retaildsds,XT1068,XT1064,XT1063,XT1069
 
 # Board
-TARGET_BOARD_INFO_FILE := device/motorola/titan/board-info.txt
+TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 
 # Kernel
-BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
+BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_PATH)/mkbootimg.mk
 TARGET_KERNEL_CONFIG := titan_defconfig
 
 # Partitions
@@ -39,8 +39,11 @@ BOARD_RECOVERYIMAGE_PARTITION_SIZE := 10526720
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1157627904
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 5912772608
 
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+
 # Recovery
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
 # inherit from the proprietary version
 -include vendor/motorola/titan/BoardConfigVendor.mk

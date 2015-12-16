22328a1063315fdc936ee17a198a0f4e331abad5
LuK1337 <priv.luk@gmail.com>
2015-12-16 21:10:25 +0100

    titan: Switch to OSS sensors HAL
    Change-Id: If3a9b59c1a9a5a4e5e473c4e9b54827408f3791f

--- device.mk
+++ device.mk
@@ -28,6 +28,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     init.target.rc
 
+# Sensors
+PRODUCT_PACKAGES += \
+    sensord
+
 # Wifi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin

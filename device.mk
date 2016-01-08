edfa1140ba5c45406f62eac0277b76afd5d7bfc5
LuK1337 <priv.luk@gmail.com>
2016-01-08 19:37:41 +0100

    titan: Implement CameraWrapper
    * Fixes issues with HDR using Snap camera app.
    
    Change-Id: I5a2c7f483a132dd15be02a9c8fdaf9934a900002

--- device.mk
+++ device.mk
@@ -24,6 +24,10 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/mixer_paths.xml:system/etc/mixer_paths.xml
 
+# Camera
+PRODUCT_PACKAGES += \
+    camera.msm8226
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.target.rc

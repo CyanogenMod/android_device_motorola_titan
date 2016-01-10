751abd656ba54b4ca152a55dc7c0a4be73e805b0
LuK1337 <priv.luk@gmail.com>
2016-01-10 14:45:13 -0500

    titan: Move camera wrapper to msm8226-common
    We are building Snap there as well.
    
    Change-Id: I4d6b7238a75d8001db826ec94387e315a05d1a48

--- device.mk
+++ device.mk
@@ -24,11 +24,6 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/mixer_paths.xml:system/etc/mixer_paths.xml
 
-# Camera
-PRODUCT_PACKAGES += \
-    camera.msm8226 \
-    Snap
-
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.target.rc
